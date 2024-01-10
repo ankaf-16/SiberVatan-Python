167764a6b99376571991d9784670fb4a81ba61f6
yunsa <155827855+ankaf-16@users.noreply.github.com>
2024-01-10 19:09:21 +0300

    first comment
    first comment

---
+++ test.py
@@ -0,0 +1,37 @@
+#yas = 10
+#YAS = 100
+#print(yas)
+
+#sayilar="10"
+#sayıları="20"
+#print(sayilar+sayıları)
+
+#maas = 1702
+#vergi = 0.20
+
+#print(1702 - (1702*0.20))
+#print(maas - (maas*vergi))
+
+#isim = "Ahmet" 
+#print("Merhaba, " + isim + " HOŞGELDİN" )
+#----------------------------------------------
+# ""==" / "bu bir birine eşitmi?"
+
+#sayı = -7 
+#print("mutlak değer" ,abs(sayı) )
+
+
+#us = pow(2,3)
+#print("Cevap", us)
+
+#from math import sqrt,ceil,floor 
+#sayı = 25
+#karekok= sqrt(sayı)
+#print("SAyının Kare kökü",karekok)
+
+
+#ondalik = 10.25
+#yukari= ceil(ondalik)
+#asagi=floor(ondalik)
+#print("yukarı",yukari)
+#print("aşağı", asagi)
