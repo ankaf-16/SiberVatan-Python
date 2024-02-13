8cf10d0660d3330ad3616a1d3ef7a9055da4a085
yunsa <155827855+ankaf-16@users.noreply.github.com>
2024-02-14 00:45:19 +0300

    ödev7

---
+++ 7-Soruluk_ödevler/rus_ruleti.py
@@ -0,0 +1,46 @@
+#ödev7 RUS RULETİ
+
+import random
+print("*-*-*-*-*-*-*RUS RULETİ OYUNUNA HOSGELDİNİZ*-*-*-*-*-*-*")
+print("OYUN NASIL OYANIR?")
+print("1 ila 6 arasında bir sayı tutulur. eğer o sayıyı secerseniz drekt tahtalı köye")
+def rasgelesayı():
+
+    return random.randint(1,6)
+
+def oyun():
+    kursun = rasgelesayı()
+   # print(kursun) #isterseniz burayı aÇın
+
+    while True:
+        kullamıcıdanalınansayı = int(input("sec: "))
+
+        if kullamıcıdanalınansayı == kursun:
+            print("öldün cık ")
+            break
+        else:
+            print("yasadın")
+            
+
+oyun()
+
+
+"""
+def oyun():
+    print(rasgelesayı())
+    rasgelesatıseç = rasgelesayı()
+
+    while True:
+        kullanıcıdanalınansayı = int(input("seç2: "))
+
+
+        if kullanıcıdanalınansayı != rasgelesatıseç:
+            print("öldün")
+            break
+        elif kullanıcıdanalınansayı == rasgelesatıseç:
+            print("yasadın")
+            break
+
+
+oyun()
+"""
