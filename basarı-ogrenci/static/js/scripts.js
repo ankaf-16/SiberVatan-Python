84c9be0563a45a064e547b8e17390e7e507a2ead
yunsa <155827855+ankaf-16@users.noreply.github.com>
2024-05-31 00:47:34 +0300

    website(basarı-ogrenci) tamamlanmış değil

---
+++ basarı-ogrenci/static/js/scripts.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Sayfa yüklendiğinde bir animasyon başlat
+    const container = document.querySelector('.container');
+    container.classList.add('animate__animated', 'animate__fadeIn');
+});
